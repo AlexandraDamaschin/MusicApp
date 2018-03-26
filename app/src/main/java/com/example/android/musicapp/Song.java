5f6ea933020233e307ad78c806fd52bd60cc03cc
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-26 11:50:25 +0100

    song class implemented

--- app/src/main/java/com/example/android/musicapp/Song.java
+++ app/src/main/java/com/example/android/musicapp/Song.java
@@ -5,4 +5,37 @@ package com.example.android.musicapp;
  */
 
 public class Song {
+    private String mNameSong;
+    private String mArtistName;
+    private static final int NO_IMAGE_PROVIDED = -1;
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    //constructor for text views and image
+    //nameSong, artistName and image
+
+    public Song(String nameSong, String artistName, int imageResourceId) {
+        mNameSong = nameSong;
+        mArtistName = artistName;
+        mImageResourceId = imageResourceId;
+    }
+
+    //    get name song
+    public String getNameSong() {
+        return mNameSong;
+    }
+
+    //  get artist name
+    public String getArtistName() {
+        return mArtistName;
+    }
+
+    //get image
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    //check if song has image or not
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
 }
