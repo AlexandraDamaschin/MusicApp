d88a8c087dbecf29b7556fe32f49ebe2eadede4b
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-26 11:28:21 +0100

    list item layout done

---
+++ app/src/main/java/com/example/android/musicapp/ListOfSongs.java
@@ -0,0 +1,12 @@
+package com.example.android.musicapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by e6420 on 3/26/2018.
+ */
+
+public class ListOfSongs  extends AppCompatActivity {
+
+
+}
