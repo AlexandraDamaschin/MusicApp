7b1af0bed8ba05fa6df08c1ef711290fd3e19945
AlexandraDamaschin <damaschin.alexandra.maria@gmail.com>
2018-03-26 12:28:26 +0100

    list of songs without picture

--- app/src/main/java/com/example/android/musicapp/ListOfSongs.java
+++ app/src/main/java/com/example/android/musicapp/ListOfSongs.java
@@ -1,12 +1,39 @@
 package com.example.android.musicapp;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 /**
  * Created by e6420 on 3/26/2018.
  */
 
-public class ListOfSongs  extends AppCompatActivity {
+public class ListOfSongs extends AppCompatActivity {
 
 
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.songs_list);
+
+        // Create a list of words
+        ArrayList<Song> songs = new ArrayList<Song>();
+
+        //songs added
+        songs.add(new Song("Sing me to sleep", "Alan Walker", R.drawable.number_one));
+        songs.add(new Song("I wanna know", "Alesso", R.drawable.number_two));
+        songs.add(new Song("I need you", "Armin van Buuren", R.drawable.number_three));
+        songs.add(new Song("Meant to be", "Bebe Rexha", R.drawable.number_four));
+        songs.add(new Song("Dancing on my own", "Calum Scott", R.drawable.number_five));
+        songs.add(new Song("You are the reason", "Calum Scott", R.drawable.number_six));
+        songs.add(new Song("IDGAK", "Dua Lipa", R.drawable.number_seven));
+        songs.add(new Song("Perfect", "Ed Sheeran", R.drawable.number_eight));
+        songs.add(new Song("Don`t", "Ed Sheeran", R.drawable.number_nine));
+        songs.add(new Song("Fire", "Gavin DeGraw", R.drawable.number_ten));
+
+//        let`s learn how to debug
+//        Log.v( "Word at index 7 is "+songs.get(7));
+
+
+    }
--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.android.musicapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //set content of the activity to use the activity_main.xml
         setContentView(R.layout.activity_main);
     }
 }
