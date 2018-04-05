b928a7a8f9fb53f96cef047a4831c813cf48d601
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-05 20:03:35 +0100

    playedSong activiti v2

--- app/src/main/java/com/example/android/musicapp/PlayedSong.java
+++ app/src/main/java/com/example/android/musicapp/PlayedSong.java
@@ -1,13 +1,17 @@
 package com.example.android.musicapp;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PlayedSong extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +36,7 @@ public class PlayedSong extends AppCompatActivity {
         //find the view that shows the play a song
         ImageView song = (ImageView) findViewById(R.id.play_song);
         //set click listener on that view
-        song.setOnClickListener(new ImageView.OnClickListener()
-
-        {
+        song.setOnClickListener(new AdapterView.OnClickListener() {
             @Override
             //intent to open songs activity
             public void onClick(View view) {
@@ -42,6 +44,10 @@ public class PlayedSong extends AppCompatActivity {
                 Intent songIntent = new Intent(PlayedSong.this, ListOfSongs.class);
                 // Start the new activity
                 startActivity(songIntent);
+
+                mMediaPlayer = MediaPlayer.create(PlayedSong.this, songs.getAudioResourceId());
+                // Start the audio file
+                mMediaPlayer.start();
             }
         });
     }
