2b467e424b73ee4e9ed111b25142b05a5502f54c
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-07 09:59:17 +0100

    final app

--- app/src/main/java/com/example/android/musicapp/ListOfSongs.java
+++ app/src/main/java/com/example/android/musicapp/ListOfSongs.java
@@ -1,13 +1,9 @@
 package com.example.android.musicapp;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
