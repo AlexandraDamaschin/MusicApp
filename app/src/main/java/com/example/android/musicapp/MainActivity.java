f42f25dee3ba5c86efff04cde677d26df930ffde
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-07 09:57:05 +0100

    label for eact activity in manifest

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -4,9 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
